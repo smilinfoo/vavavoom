da91c58c1c511b92c2bee5f93e84350e4d0bb0d6
smilinfoo <jeff@jcgray.com>
2025-01-20 10:51:39 -0500

    fix error

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,7 +2,7 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-
+/*
 export default async function LatestInvoices({
   latestInvoices,
 }: {
@@ -14,6 +14,7 @@ export default async function LatestInvoices({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+    */
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
         {/* <div className="bg-white px-6">
@@ -53,7 +54,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div> */}/*
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
@@ -62,3 +63,4 @@ export default async function LatestInvoices({
     </div>
   );
 }
+*/
